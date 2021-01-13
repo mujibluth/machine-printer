2a48fedba15bd300cd9adb62c9813bb8e288504f
3p1c_5H3L73R <abdulmujiburrohmanluthfi123@gmail.com>
2021-01-13 23:06:47 +0700

    update tugas.py
    add 'cek ketersediaan kertas'

--- tugas.py
+++ tugas.py
@@ -2,6 +2,24 @@ import tkinter as tk
 
 master = tk.Tk()
 master.title("Project Akhir")
-master.geometry("250x100")
+master.geometry("300x300")
+
+# cek ketersediaan kertas
+tk.Label(master, text="Check Paper Availability", font = ('Google Sans','13', 'bold')).grid(row=0, column=0, columnspan = 4)
+tk.Label(master, text="A4", font = ('Google Sans','10')).grid(row=1, column=0, sticky=tk.E)
+tk.Entry(master, justify="left").grid(row=1, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
+tk.Button(master, text = "add").grid(row=1, column = 3, sticky=tk.W)
+
+tk.Label(master, text="A3", font = ('Google Sans','10')).grid(row=2, column=0, sticky=tk.E)
+tk.Entry(master, justify="left").grid(row=2, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
+tk.Button(master, text = "add").grid(row=2, column = 3, sticky=tk.W)
+
+tk.Label(master, text="B1", font = ('Google Sans','10')).grid(row=3, column=0, sticky=tk.E)
+tk.Entry(master, justify="left").grid(row=3, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
+tk.Button(master, text = "add").grid(row=3, column = 3, sticky=tk.W)
+
+tk.Label(master, text="B2", font = ('Google Sans','10')).grid(row=4, column=0, sticky=tk.E)
+tk.Entry(master, justify="left").grid(row=4, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
+tk.Button(master, text = "add").grid(row=4, column = 3, sticky=tk.W)
 
 tk.mainloop()
