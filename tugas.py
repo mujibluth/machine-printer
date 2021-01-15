5e1d68afe93b8cc8ad76f5ca0e6d9f4cfadbb7d1
Aldi <76525619+Aldiasa@users.noreply.github.com>
2021-01-16 00:46:32 +0700

    Update tugas.py
    code untuk cek ketersediaan Tinta

--- tugas.py
+++ tugas.py
@@ -22,4 +22,10 @@ tk.Label(master, text="B2", font = ('Google Sans','10')).grid(row=4, column=0, s
 tk.Entry(master, justify="left").grid(row=4, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
 tk.Button(master, text = "add").grid(row=4, column = 3, sticky=tk.W)
 
+# cek ketersediaan Tinta
+tk.Label(master, text="Check Ink Availability", font = ('Google Sans','13', 'bold')).grid(row=5, column=0, columnspan = 4, sticky = tk.W)
+tk.Label(master, text="Dark", font = ('Google Sans','10')).grid(row=6, rowspan = 2, column=0, sticky=tk.E+tk.N)
+tk.Entry(master, justify="left").grid(row=6, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
+tk.Button(master, text = "add").grid(row=6, column = 3, sticky=tk.W)
+
 tk.mainloop()
